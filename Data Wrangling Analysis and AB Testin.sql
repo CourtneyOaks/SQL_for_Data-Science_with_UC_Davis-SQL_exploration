e1f4d477c8c9aa6c86b56d33b428e19ecd23f2ff
Courtney Oaks <102244119+CourtneyOaks@users.noreply.github.com>
2023-07-11 16:15:04 -0500

    Update and rename -- Data Wrangling Analysis and AB Testin.sql to Data Wrangling Analysis and AB Testin.sql

--- Data Wrangling Analysis and AB Testin.sql
+++ Data Wrangling Analysis and AB Testin.sql
@@ -1,5 +1,6 @@
--- Data Wrangling Analysis and AB Testing: Assignment Tasks --
+-- Data Wrangling Analysis and AB Testing:
 
+Assignment Tasks --
 -- We are running an experiment at an item-level, which means all users who visit will see the same page, but the layout of different item pages may differ. 
 -- Please follow the steps below and good luck!
 
